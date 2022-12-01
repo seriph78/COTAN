291737b07ea831fd29acb50976a926bf0c99bf07
trinetra75 <marco.fantozzi@gmail.com>
2022-12-02 00:01:53 +0100

    Adapted test to the new method signature
    Now cell_homogeneous_cluestering is handled accordingly to the
    new signature of the method in the tests.

--- tests/testthat/test-COTAN_main.R
+++ tests/testthat/test-COTAN_main.R
@@ -186,13 +186,13 @@ test_that("mat2vec_rfast_test", {
 
 
 test_that("7_cell_homogeneous_clustering", {
-  #obj <- readRDS(file.path(tm,"temp.RDS"))
-  temp <- cell_homogeneous_clustering(cond = "test",out_dir = paste0(tm,"/"), in_dir = paste0(tm,"/"),
-                                      cores = 12,
-                                      dataset_name = "temp.RDS",
-                                      GEO = "test", sc.method ="10X"
-                                      )
+  obj <- readRDS(file.path(tm,"temp.RDS"))
+  clusters <- cell_homogeneous_clustering(obj, cond = "test", cores = 12,
+                                          out_dir = paste0(tm,"/"))
+
+  obj <- addClusterization(obj, clusterizationName = "clusters", clusters = clusters)
   saveRDS(temp, file = file.path(tm, "temp.RDS"))
+
   #clusters <- readRDS(file.path(getwd(),"clusters1.RDS"))
 
   #expect_equal(temp@clusters, clusters)
@@ -201,17 +201,17 @@ test_that("7_cell_homogeneous_clustering", {
   # Test the low GDI (homogeneity) for each defined clusters
 
   ####################################
+  expect_equal(getClusterizationData(obj)[["clusters"]], clusters)
 
-  for (cl in sample(unique(temp@clusters),size = 5)) {
-    cells.to_test <-  names(temp@clusters[temp@clusters == cl])
+  for (cl in sample(unique(clusters), size = 5)) {
+    cells.to_test <-  clusters == cl
+
     #temp.obj <- cluster_homogeneity_check(obj = obj,cells = cells.to_test,
     #                                      out_dir = paste0(tm,"/"),
     #                                      cores = cores,
     #                                      code = 12)
 
-    temp.obj <- temp@raw[,colnames(temp@raw) %in% cells.to_test]
-
-    temp.obj <- COTAN(raw = temp.obj)
+    temp.obj <- COTAN(getRawData(obj)[, cells.to_test, drop = FALSE])
     temp.obj <- initializeMetaDataset(temp.obj, GEO = "",
                                       sequencingMethod = " ",
                                       sampleCondition = "temp.clustered")
@@ -219,8 +219,6 @@ test_that("7_cell_homogeneous_clustering", {
     temp.obj <- clean(temp.obj, calcExtraData = FALSE)[["objCOTAN"]]
 
     temp.obj <- estimateDispersion(temp.obj, cores = 12)
-    gc()
-
     temp.obj <- calculateCoex(temp.obj)
     gc()
 
@@ -248,8 +246,6 @@ test_that("7_cell_homogeneous_clustering", {
 #})
 
 
-
-
 test_that("8_merge_cell.clusters.test", {
   temp <- readRDS(file.path(tm,"temp.RDS"))
   temp <- DEA_on_clusters(as(temp,"scCOTAN"))[[1]]
@@ -258,11 +254,10 @@ test_that("8_merge_cell.clusters.test", {
   temp <- merge_cell.clusters(obj = temp,
                               cond = "test",
                               cores=12,
-                              #out_dir_root = paste0(tm,"/"),
                               srat = "Seurat_obj_test_with_cotan_clusters.RDS" ,
                               out_dir = paste0(tm,"/") ,
                               GEO = "test",
-                              sc.method = "10X")#,mt = FALSE, mt_prefix="^MT")
+                              sc.method = "10X")
 
   final.cluster.number <- dim(temp@cluster_data)[2]
   expect_true(final.cluster.number < initial.cluster.number)
