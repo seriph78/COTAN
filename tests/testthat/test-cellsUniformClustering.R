122d07c124df4542d7ea8c90a0bf5d5e8e7200d2
Marco Fantozzi <57561972+trinetra75@users.noreply.github.com>
2023-05-08 15:44:41 +0200

    Added cases with saveObj == TRUE to some tests (#18)

--- tests/testthat/test-cellsUniformClustering.R
+++ tests/testthat/test-cellsUniformClustering.R
@@ -9,13 +9,13 @@ test_that("Cell Uniform Clustering", {
                                sequencingMethod = "artificial",
                                sampleCondition = "test")
 
-  obj <- proceedToCoex(obj, cores = 12, saveObj = FALSE)
+  obj <- proceedToCoex(obj, cores = 12, saveObj = TRUE, outDir = tm)
 
   GDIThreshold <- 1.5
 
   clusters <- cellsUniformClustering(obj, cores = 12,
                                      GDIThreshold = GDIThreshold,
-                                     saveObj = FALSE, outDir = tm)
+                                     saveObj = TRUE, outDir = tm)
 
   gc()
 
