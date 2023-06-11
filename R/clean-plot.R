4a78911672cf90160a66171bfe9a11dfa4144b2d
trinetra75 <marco.fantozzi@gmail.com>
2023-06-11 02:45:50 +0200

    Added zoomed UDE plot
    Now the cleanPlots() function returns also the UDE plot zoomed
    on the left to help decide whether and at which nu level to cut
    the cells with lowest efficiency as outliers.
    
    The plot also suggests a possible threshold.

--- R/clean-plot.R
+++ R/clean-plot.R
@@ -4,11 +4,13 @@
 #' @param objCOTAN a `COTAN` object
 #'
 #' @returns `cleanPlots()` returns a `list` of `ggplot2` plots:
-#'   * "pcaCells" is for pca cells,
-#'   * "pcaCellsData" is the data of the pca cells,
-#'   * "genes" is for cluster2 cells' group genes,
-#'   * "UDE" is for cell UDE,
-#'   * "nu" is for cell nu.
+#'   * `"pcaCells"` is for pca cells
+#'   * `"pcaCellsData"` is the data of the pca cells (can be plotted)
+#'   * `"genes"` is for `B` group cells' genes
+#'   * `"UDE"` is for cells' UDE against their pca
+#'   * `"nu"` is for cell *nu*
+#'   * `"zoomedNu"` is the same but zoomed on the left and with an estimate
+#'      for the low *nu* threshold that defines problematic cells
 #'
 #' @export
 #'
@@ -41,6 +43,8 @@
 #' @importFrom ggplot2 ylab
 #' @importFrom ggplot2 scale_color_gradient2
 #' @importFrom ggplot2 scale_color_manual
+#' @importFrom ggplot2 xlim
+#' @importFrom ggplot2 ylim
 #' @importFrom ggplot2 geom_label
 #' @importFrom ggrepel geom_text_repel
 #'
@@ -51,9 +55,10 @@
 #' # This creates many infomative plots useful to determine whether
 #' # there is still something to drop...
 #' # Here we use the tuple-like assignment feature of the `zeallot` package
-#' c(pcaCellsPlot, ., genesPlot, UDEPlot, nuPlot ) %<-% cleanPlots(objCOTAN)
+#' c(pcaCellsPlot, ., genesPlot, UDEPlot, ., zNuPlot) %<-% cleanPlots(objCOTAN)
 #' plot(pcaCellsPlot)
 #' plot(UDEPlot)
+#' plot(zNuPlot)
 #'
 #' @rdname RawDataCleaning
 #'
@@ -137,41 +142,64 @@ cleanPlots <- function(objCOTAN) {
 
   minN <- min(D[["n"]]) + 15.0
   lowD <- D[["n"]] < minN
-  genesPlot <-  ggplot(D, aes(x = n, y = means)) +
-                geom_point() +
-                ggtitle(label = "B cell group genes mean expression"
-                        #subtitle = " - B group NOT removed -"
-                        ) +
-                geom_label(data = subset(D, lowD),
-                      aes(x = n, y = means, label = rownames(D)[lowD]),
-                      #nudge_y = 0.05,
-                      nudge_x = 400.0
-                      #direction = "x",
-                      #angle = 90.0,
-                      #vjust = 0L,
-                      #segment.size = 0.2
-                      ) +
-                plotTheme("genes") +
+  genesPlot <- ggplot(D, aes(x = n, y = means)) +
+               geom_point() +
+               ggtitle(label = "B cell group genes mean expression"
+                       #subtitle = " - B group NOT removed -"
+                       ) +
+               geom_label(data = subset(D, lowD),
+                     aes(x = n, y = means, label = rownames(D)[lowD]),
+                     #nudge_y = 0.05,
+                     nudge_x = 400.0
+                     #direction = "x",
+                     #angle = 90.0,
+                     #vjust = 0L,
+                     #segment.size = 0.2
+                     ) +
+               plotTheme("genes") +
   theme(plot.title = element_text(hjust = 1.0),
         plot.subtitle = element_text(hjust = 0.95, vjust = -25.0))
 
 
   nuEst <- round(getNu(objCOTAN), digits = 7L)
-  UDEPlot      <- ggplot(pcaCells, aes(x = PC1, y = PC2, colour = log(nuEst))) +
-                  geom_point(size = 1L, alpha = 0.8) +
-                  scale_color_gradient2(low = "#E64B35B2", mid = "#4DBBD5B2",
-                                        high = "#3C5488B2",
-                                        midpoint = log(mean(nuEst)),
-                                        name = "ln(nu)") +
-                  ggtitle("Cells PCA coloured by cells efficiency") +
-                  plotTheme("UDE")
+  UDEPlot <- ggplot(pcaCells, aes(x = PC1, y = PC2, colour = log(nuEst))) +
+             geom_point(size = 1L, alpha = 0.8) +
+             scale_color_gradient2(low = "#E64B35B2", mid = "#4DBBD5B2",
+                                   high = "#3C5488B2",
+                                   midpoint = log(mean(nuEst)),
+                                   name = "ln(nu)") +
+             ggtitle("Cells PCA coloured by cells efficiency") +
+             plotTheme("UDE")
 
   nuDf <- data.frame("nu" = sort(getNu(objCOTAN)),
                      "n" = seq_len(getNumCells(objCOTAN)))
-  nuPlot       <- ggplot(nuDf, aes(x = n, y = nu)) +
-                  geom_point(colour = "#8491B4B2", size = 1L) +
-                  plotTheme("common")
+  nuPlot <- ggplot(nuDf, aes(x = n, y = nu)) +
+            geom_point(colour = "#8491B4B2", size = 1L) +
+            plotTheme("common")
+
+  nuDf <- nuDf[1:(min(400L, nrow(nuDf))), ]
+  zNuPlot <- ggplot(nuDf, aes(x = n, y = nu)) +
+             geom_point(colour = "#8491B4B2", size = 1L) +
+             plotTheme("common") +
+             xlim(0, nrow(nuDf)) +
+             ylim(0.0, round(max(nuDf[["nu"]]) + 0.05, digits = 2L))
+
+  # estimate the elbow point if any...
+  secondDer <- diff(nuDf[1:min(100L, nrow(nuDf)), "nu"], differences = 2)
+  if (min(secondDer) < -0.01) {
+    lowUDEThr <- nuDf[(max(which(secondDer < -0.01)) + 1), "nu"] - 0.005
+    zNuPlot <- zNuPlot +
+               geom_hline(yintercept = lowUDEThr, linetype = "dashed",
+                          color = "darkred") +
+               annotate(geom = "text",
+                        x = (nrow(nuDf) / 2.0),
+                        y = (lowUDEThr - 0.05),
+                        label = paste0("cells with nu < ", lowUDEThr,
+                                       " should probably be removed"),
+                        color = "darkred", size = 4.5)
+  }
 
   return(list("pcaCells" = pcaCellsPlot, "pcaCellsData" = pcaCells,
-              "genes" = genesPlot, "UDE" = UDEPlot, "nu" = nuPlot))
+              "genes" = genesPlot, "UDE" = UDEPlot,
+              "nu" = nuPlot, "zoomedNu" = zNuPlot))
 }
