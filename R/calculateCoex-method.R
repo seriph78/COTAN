b2e0cfc28b3bb0137846f684d878388c387a95a7
trinetra75 <marco.fantozzi@gmail.com>
2023-06-22 20:40:41 +0200

    Added function `calculateGenesCE()`
    It returns the cross-entropy between the expected absence of
    ritzs reading against the observed state
    
    Moved the functions `calculateGDI()`, `calculatePValue()` to
    a new file. Added `calculatePDI` too.

--- R/calculateCoex-method.R
+++ R/calculateCoex-method.R
@@ -159,8 +159,6 @@ observedContingencyTablesYY <- function(objCOTAN,
 observedContingencyTables <- function(objCOTAN,
                                       actOnCells = FALSE,
                                       asDspMatrices = FALSE) {
-  zeroOne <- getZeroOneProj(objCOTAN)
-
   numGenes <- getNumGenes(objCOTAN)
   numCells <- getNumCells(objCOTAN)
 
@@ -199,7 +197,6 @@ observedContingencyTables <- function(objCOTAN,
 
     observedNY <- as(observedNY, "denseMatrix")
   }
-  rm(zeroOne)
   gc()
   logThis(" done", logLevel = 3L)
 
@@ -351,13 +348,6 @@ expectedContingencyTables <- function(objCOTAN,
                                       actOnCells = FALSE,
                                       asDspMatrices = FALSE,
                                       optimizeForSpeed = TRUE) {
-  # estimate Probabilities of 0 with internal function funProbZero
-  probZero <- funProbZero(getDispersion(objCOTAN), calculateMu(objCOTAN))
-
-  assert_that(!anyNA(probZero),
-              msg = paste0("Error: some NA in matrix of probability",
-                           " of zero UMI counts"))
-
   numGenes <- getNumGenes(objCOTAN)
   numCells <- getNumCells(objCOTAN)
 
@@ -397,7 +387,6 @@ expectedContingencyTables <- function(objCOTAN,
 
     expectedYN <- as(expectedYN, "denseMatrix")
   }
-  rm(probZero)
   gc()
 
   expectedYY <- forceSymmetric(as(expectedYY, "denseMatrix"))
@@ -746,139 +735,4 @@ calculateG <- function(objCOTAN, geneSubsetCol = vector(mode = "character"),
   logThis("Calculating G: DONE", logLevel = 2L)
 
   return(G)
-}
-
-
-#' @details `calculateGDI()` produces a`data.frame` with the *GDI* for each gene
-#'
-#' @param objCOTAN a `COTAN` object
-#' @param statType Which statistics to use to compute the p-values. By default
-#'   it will use the `S` (*Pearson's *\eqn{\chi^{2}}* test*) otherwise the `G`
-#'   (*G-test*)
-#'
-#' @returns `calculateGDI()` returns a `data.frame` with the *GDI* data
-#'
-#' @export
-#'
-#' @importFrom rlang set_names
-#'
-#' @importFrom stats pchisq
-#'
-#' @importFrom Matrix rowSums
-#' @importFrom Matrix colMeans
-#' @importFrom Matrix forceSymmetric
-#'
-#' @rdname CalculatingCOEX
-#'
-calculateGDI <- function(objCOTAN, statType = "S") {
-  if (statType == "S") {
-    logThis("Using S", logLevel = 3L)
-    S <- calculateS(objCOTAN)
-  } else if (statType == "G") {
-    logThis("Using G", logLevel = 3L)
-    S <- calculateG(objCOTAN)
-  } else {
-    stop("Unrecognised stat type: must be either 'S' or 'G'")
-  }
-
-  logThis("Calculate GDI dataframe: START", logLevel = 2L)
-
-  top5pcRows <- as.integer(max(1L:round(getNumGenes(objCOTAN) / 20.0,
-                                        digits = 0L)))
-
-  pValueSorted <- apply(S, 2L, sort, decreasing = TRUE)
-  pValueSorted <- pValueSorted[1L:top5pcRows, , drop = FALSE]
-  pValueSorted <- pchisq(as.matrix(pValueSorted), df = 1L, lower.tail = FALSE)
-
-  GDI <- log(-log(colMeans(pValueSorted)))
-  GDI <- set_names(as.data.frame(GDI), "GDI")
-  rm(pValueSorted)
-  gc()
-
-  sumRawNorm <- log(rowSums(getNormalizedData(objCOTAN)))
-  GDI <- merge(GDI, as.data.frame(list(sumRawNorm), col.names = "sum.raw.norm"),
-               by = "row.names", all.x = TRUE)
-  GDI <- column_to_rownames(GDI, var = "Row.names")
-  rm(sumRawNorm)
-  gc()
-
-  expCells <- getNumOfExpressingCells(objCOTAN) / getNumCells(objCOTAN) * 100.0
-  GDI <- merge(GDI, as.data.frame(list(expCells), col.names = "exp.cells"),
-               by = "row.names", all.x = TRUE)
-  GDI <- column_to_rownames(GDI, var = "Row.names")
-  rm(expCells)
-  gc()
-
-  GDI <- GDI[, c("sum.raw.norm", "GDI", "exp.cells")]
-
-  logThis("Calculate GDI dataframe: DONE", logLevel = 2L)
-
-  return(GDI)
-}
-
-
-#' @details `calculatePValue()` computes the p-values for genes in the `COTAN`
-#'   object. It can be used genome-wide or by setting some specific genes of
-#'   interest. By default it computes the *p-values* using the `S` statistics
-#'   (\eqn{\chi^{2}})
-#'
-#' @param objCOTAN a `COTAN` object
-#' @param statType Which statistics to use to compute the p-values. By default
-#'   it will use the "S" (Pearson's \eqn{\chi^{2}} test) otherwise the "G"
-#'   (G-test)
-#' @param geneSubsetCol an array of genes. It will be put in columns. If left
-#'   empty the function will do it genome-wide.
-#' @param geneSubsetRow an array of genes. It will be put in rows. If left empty
-#'   the function will do it genome-wide.
-#'
-#' @return `calculatePValue()` returns a *p-value* `matrix` as `dspMatrix`
-#'
-#' @export
-#'
-#' @importFrom Matrix forceSymmetric
-#' @importFrom Matrix pack
-#'
-#' @importClassesFrom Matrix dspMatrix
-#'
-#' @rdname CalculatingCOEX
-#'
-calculatePValue <- function(objCOTAN, statType = "S",
-                            geneSubsetCol = vector(mode = "character"),
-                            geneSubsetRow = vector(mode = "character")) {
-  geneSubsetCol <- handleNamesSubsets(getGenes(objCOTAN), geneSubsetCol)
-  geneSubsetRow <- handleNamesSubsets(getGenes(objCOTAN), geneSubsetRow)
-
-  if (statType == "S") {
-    logThis("Using S", logLevel = 3L)
-    S <- calculateS(objCOTAN,
-                    geneSubsetCol = geneSubsetCol,
-                    geneSubsetRow = geneSubsetRow)
-  } else if (statType == "G") {
-    logThis("Using G", logLevel = 3L)
-    S <- calculateG(objCOTAN,
-                    geneSubsetCol = geneSubsetCol,
-                    geneSubsetRow = geneSubsetRow)
-  } else {
-    stop("Unrecognised stat type: must be either 'S' or 'G'")
-  }
-
-  logThis("calculating PValues: START", logLevel = 2L)
-
-  allCols <- identical(geneSubsetCol, getGenes(objCOTAN))
-  allRows <- identical(geneSubsetRow, getGenes(objCOTAN))
-
-  strCol <- if (allCols) "genome wide" else "on a set of genes"
-  strRow <- if (allRows) "genome wide" else "on a set of genes"
-  logThis(paste("Get p-values", strCol, "on columns and", strRow, "on rows"),
-          logLevel = 2L)
-
-  pValues <- pchisq(as.matrix(S), df = 1L, lower.tail = FALSE)
-
-  if (allCols && allRows) {
-    pValues <- pack(forceSymmetric(pValues))
-  }
-
-  logThis("calculating PValues: DONE", logLevel = 2L)
-
-  return(pValues)
 }
